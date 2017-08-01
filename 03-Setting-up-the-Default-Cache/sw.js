6530a45528462d9e092e151aafbd6560f5a93fdf
kelly <kelly@dgfactor.com.tw>
2017-08-01 15:56:38 +0800

    InstallEvent 及 儲存陣列。

---
+++ 03-Setting-up-the-Default-Cache/sw.js
@@ -0,0 +1,31 @@
+/* 01)
+ * 建立一個陣列，名稱為 filesToCache，這個陣列中會存放所有靜態檔案的 URL。
+ */
+const filesToCache = [
+	'/',
+	'/index.html'
+];
+
+/* 02) install
+ * 使用 waitUntil 方法去等待 cache 成功後再進入到下一個階段。
+ * 自訂一個 cache 名稱為 static-v1，透過 caches.open 取得 static-v1 的 cache object，
+ * 再把我們前面定義的陣列(filesToCache) 存入 cache object。
+ */
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open('static-v1').then(cache => {
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+/* 03) 這時就可以透過 chrome 的 devtools （Application > Cache Storage）看到我們儲存的資料 */
+
+// activate
+self.addEventListener('activate', event => {
+    console.log('now ready to handle fetches!');
+});
+
+// fetch
+self.addEventListener('fetch', event => {
+    console.log('now fetch!');
+});
